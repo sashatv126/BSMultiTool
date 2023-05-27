f022322644d6c7a3a2688c0b87896c943cf40972
sasha <sashatv126@gmail.com>
2023-05-27 20:42:08 +0300

    update

--- Package.swift
+++ Package.swift
@@ -1,6 +1,5 @@
-// swift-tools-version: 5.7
+// swift-tools-version:5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
-
 
 import PackageDescription
 
@@ -23,9 +22,10 @@ let package = Package(
             dependencies: [
                 .product(name: "SnapKit", package: "SnapKit")
             ],
-            path: "Sources/BSMultiTool", // Обновленный путь к источникам вашей основной цели
+            path: "Sources",
             sources: [
-                "Extensions" // Обновленный путь к источникам вашего расширения
+                "BSMultiTool",
+                "Extensions"
             ]
         )
     ]
